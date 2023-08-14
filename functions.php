2f39207e7c76d7532eb560ee8ae748996a8f1969
takayuki-takahashi-dic <ttkaws15@gmail.com>
2023-08-15 08:04:42 +0900

    テンプレートの分割 / リソースの一元化

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+function my_enqueue_scripts()
+{
+	$uri = esc_url(get_template_directory_uri());
+	wp_enqueue_script('jquery');
+	wp_enqueue_script('bundle_js', $uri . '/assets/js/bundle.js', array());
+	wp_enqueue_style('my_styles', $uri . '/assets/css/styles.css', array());
+}
+add_action('wp_enqueue_scripts', 'my_enqueue_scripts');
