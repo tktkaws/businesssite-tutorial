aaba36e1fd73acbe03bc083090e9512f1d368a29
takayuki-takahashi-dic <ttkaws15@gmail.com>
2023-08-16 08:06:29 +0900

    page.phpを作成

--- footer.php
+++ footer.php
@@ -1,3 +1,10 @@
+<?php if( ! is_front_page() ): ?>
+</div>
+</div>
+</main>
+</div>
+</div>
+<?php endif; ?>
 <footer class="footer" id="footer">
     <div class="footerContents">
         <div class="footerContents-contact">
@@ -15,32 +22,12 @@
         </div>
         <div class="footerContents-sitemap">
             <nav class="footer-nav">
-                <ul class="menu">
-                    <li class="menu-item">
-                        <a class="nav-link" href="#">企業情報</a>
-                    </li>
-                    <li class="menu-item">
-                        <a class="nav-link" href="#">会社概要</a>
-                    </li>
-                    <li class="menu-item">
-                        <a class="nav-link" href="#">事業紹介</a>
-                    </li>
-                    <li class="menu-item">
-                        <a class="nav-link" href="#">沿革</a>
-                    </li>
-                    <li class="menu-item">
-                        <a class="nav-link" href="#">店舗情報</a>
-                    </li>
-                    <li class="menu-item">
-                        <a class="nav-link" href="#">地域貢献活動</a>
-                    </li>
-                    <li class="menu-item">
-                        <a class="nav-link" href="#">ニュースリリース</a>
-                    </li>
-                    <li class="menu-item">
-                        <a class="nav-link" href="#">お問い合わせ</a>
-                    </li>
-                </ul>
+                <?php
+wp_nav_menu( array(
+  'theme_location' => 'place_footer',
+  'container' => false,
+) );
+?>
             </nav>
         </div>
     </div>
--- header.php
+++ header.php
@@ -8,24 +8,21 @@
     <meta name="description" content="<?php bloginfo('description'); ?>" />
     <title><?php echo esc_html(wp_get_document_title()); ?></title>
 
-    <link rel="shortcut icon"
-        href="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/common/favicon.ico" />
+    <link rel="shortcut icon" href="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/common/favicon.ico" />
     <link href="https://fonts.googleapis.com/earlyaccess/notosansjapanese.css" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css?family=Vollkorn:400i" rel="stylesheet" />
     </script>
     <?php wp_head(); ?>
 </head>
 
-<body>
+<body <?php body_class(); ?>>
     <div class="container">
         <header id="header">
             <div class="header-inner">
                 <div class="logo">
                     <a class="logo-header" href="/pacificmall">
-                        <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/common/logo-main.svg"
-                            class="main-logo" alt="PACIFIC MALL DEVELOPMENT" />
-                        <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/common/logo-fixed.svg"
-                            class="fixed-logo" alt="PACIFIC MALL DEVELOPMENT" />
+                        <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/common/logo-main.svg" class="main-logo" alt="PACIFIC MALL DEVELOPMENT" />
+                        <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/common/logo-fixed.svg" class="fixed-logo" alt="PACIFIC MALL DEVELOPMENT" />
                     </a>
                 </div>
                 <button class="toggle-menu js-toggoleNav">
@@ -67,15 +64,34 @@
                 </div>
             </div>
         </header>
-        <section class="section-contents" id="keyvisual">
-            <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/bg-section-keyvisual.jpg"
-                alt="MAIN IMAGE" />
-            <div class="wrapper">
-                <h1 class="site-title">Connecting the future.</h1>
-                <p class="site-caption">
-                    私たちパシフィックモール開発は<br />
-                    世界各地のショッピングモール開発を通じて<br />
-                    人と人、人と地域を結ぶお手伝いをしています。
-                </p>
-            </div>
-        </section>
+        <?php if (is_front_page()) : ?>
+            <section class="section-contents" id="keyvisual">
+                <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/bg-section-keyvisual.jpg" alt="MAIN IMAGE" />
+                <div class="wrapper">
+                    <h1 class="site-title">Connecting the future.</h1>
+                    <p class="site-caption">
+                        私たちパシフィックモール開発は<br />
+                        世界各地のショッピングモール開発を通じて<br />
+                        人と人、人と地域を結ぶお手伝いをしています。
+                    </p>
+                </div>
+            </section>
+        <?php else : ?>
+            <div class="wrap">
+                <div id="primary" class="content-area">
+                    <main>
+                        <div class="page-contents">
+                            <div class="page-head">
+                                <!-- imgタグ出力 -->
+                                <div class="wrapper">
+                                    <span class="page-title-en"></span>
+                                    <h2 class="page-title"><?php echo get_main_title(); ?></h2>
+                                    <h2 class="page-title">
+                                        <!-- ページタイトル出力 -->
+
+
+                                    </h2>
+                                </div>
+                            </div>
+                            <div class="page-container">
+                            <?php endif; ?>
