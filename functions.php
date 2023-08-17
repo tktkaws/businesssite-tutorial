ceaf69e124131fb851b32677b21fffd6f9bd2483
takayuki-takahashi-dic <ttkaws15@gmail.com>
2023-08-17 10:39:45 +0900

    サブクエリで企業情報一覧を表示

--- functions.php
+++ functions.php
@@ -26,3 +26,22 @@ function get_main_title()
 		return single_cat_title();
 	endif;
 }
+
+// 子ページを取得する関数
+function get_child_pages($number = -1, $specified_id = null)
+{
+	if (isset($specified_id)) :
+		$parent_id = $specified_id;
+	else :
+		$parent_id = get_the_ID();
+	endif;
+	$args = array(
+		'posts_per_page' => $number,
+		'post_type' => 'page',
+		'orderby' => 'menu_order',
+		'order' => 'ASC',
+		'post_parent' => $parent_id,
+	);
+	$child_pages = new WP_Query($args);
+	return $child_pages;
+}
