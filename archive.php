bcd65d81599d9aa9a12d8cbccf4f348f071d2e18
takayuki-takahashi-dic <ttkaws15@gmail.com>
2023-08-21 07:52:58 +0900

    ページャーの設置

--- archive.php
+++ archive.php
@@ -1,19 +1,26 @@
 <?php get_header(); ?>
-			  <div class="page-inner full-width">
-                <div class="page-main" id="pg-news">
-                  <div class="main-container">
-                    <div class="main-wrapper">
-                      <div class="newsLists">
-<?php
-if ( have_posts() ):
-	while ( have_posts() ) : the_post();
-		get_template_part( 'content-archive' );
-	endwhile;
-endif;
-?>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+<div class="page-inner full-width">
+  <div class="page-main" id="pg-news">
+    <div class="main-container">
+      <div class="main-wrapper">
+        <div class="newsLists">
+          <?php
+          if (have_posts()) :
+            while (have_posts()) : the_post();
+              get_template_part('content-archive');
+            endwhile;
+          endif;
+          ?>
+        </div>
+        <div class="pager">
+          <ul class="pagerList">
+            <?php
+            page_navi();
+            ?>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 <?php get_footer(); ?>
